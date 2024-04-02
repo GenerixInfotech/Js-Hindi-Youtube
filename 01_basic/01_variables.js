2285ac976ea928b7bf114c6122b94279858a5d8d
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2024-04-02 06:41:57 +0000

    variables done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 112233
+let accountEmail = "manish@google.com"
+var accountPassword = "223311"
+accountCity = "Jamshedpur"
+
+console.table([accountId, accountEmail, accountPassword])
